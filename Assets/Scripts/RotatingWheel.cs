65e809b762d5cfc1b8e7e788a4d110cf806a6a59
Stijn van Sprang <stijnvansprang@hotmail.com>
2023-11-28 19:22:13 +0100

    Added rotation code generator

--- Assets/Scripts/RotatingWheel.cs
+++ Assets/Scripts/RotatingWheel.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotatingWheel : MonoBehaviour
 {
+    public float rotationSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class RotatingWheel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
     }
 }
